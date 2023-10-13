c00dddcfed0a98f183b6a0560675666c9a15d26e
Naufal7723 <naufalff128@gmail.com>
2023-10-13 11:36:27 +0700

    comitsepen

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NewComponent from './newComponent'; // Sesuaikan dengan path yang benar
+
+function App() {
+  return (
+    <div>
+      <h1>Ini adalah teks dari App component!</h1>
+      <NewComponent /> {/* Gunakan komponen NewComponent di sini */}
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/newComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NewComponent() {
+  return (
+    <div>
+      <h1>Ini adalah teks dari New Component!</h1>
+    </div>
+  );
+}
+
+export default NewComponent;
